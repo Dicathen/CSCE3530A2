ecc22d49c2a4d8e1a54f5f7dfaffe31eb5fe7a99
Alex Ho <hoalex219@gmail.com>
2024-05-05 20:16:25 -0500

    Reverse proxy and other required files

---
+++ README.md
@@ -0,0 +1,19 @@
+# Network Programming Assignment
+Simple server/client/proxy network programming assignment for CSCE3530
+
+## Usage
+1. If you don't have the code already, clone the repo
+
+    `git clone https://github.com/Dicathen/CSCE3530A2.git`
+2. To run the server, use the following command:
+
+    `cargo run server <port>`
+
+3. To run the client, use the following command:
+
+    `cargo run client <port>`
+4. To run the proxy, use the following command:
+
+    `cargo run proxy <port>`
+
+## NOTE, IF USING THE PROXY, THE SERVERS HAVE TO USE PORT 8001 AND 8002
--- src/main.rs
+++ src/main.rs
@@ -57,8 +57,61 @@ fn main() {
             }
         }
     }
+    else if args[1] == "proxy" {
+        //proxies two servers running on port 8001 and 8002
+        let port = args[2].parse::<u16>()
+            .expect("Could not parse port number");
+        let socket = init_proxy(port);
+        let server1 = net::SocketAddr::from(([127, 0, 0, 1], 8001));
+        let server2 = net::SocketAddr::from(([127, 0, 0, 1], 8002));
+        let mut index = 1;
+        loop {
+            let mut buf = [0; 256];
+            let (amt, src) = socket.recv_from(&mut buf)
+                .expect("Could not receive data");
+            if &buf[..amt] == b"PING" {
+                if index == 1 {
+                    socket.send_to(b"PING", &server1)
+                        .expect("Could not send data");
+                    match socket.recv(&mut buf) {
+                        //send the PONG with port number and index of the server
+                        Ok(amt) => {
+                            let pong = format!("{} from: {} index: {}", std::str::from_utf8(&buf[..amt]).unwrap(), 8001, index);
+                            socket.send_to(pong.as_bytes(), &src)
+                                .expect("Could not send data");
+                        }
+                        Err(ref e) => if e.kind() == std::io::ErrorKind::WouldBlock {
+                            println!("[Proxy] Timed out");
+                        }
+                        else {
+                            eprint!("Error: {:?}", e);
+                        }
+                    }
+                    index = 2;
+                }
+                else {
+                    socket.send_to(b"PING", &server2)
+                        .expect("Could not send data");
+                    match socket.recv(&mut buf) {
+                        Ok(amt) => {
+                            let pong = format!("{} from: {} index: {}", std::str::from_utf8(&buf[..amt]).unwrap(), 8002, index);
+                            socket.send_to(pong.as_bytes(), &src)
+                                .expect("Could not send data");
+                        }
+                        Err(ref e) => if e.kind() == std::io::ErrorKind::WouldBlock {
+                            println!("[Proxy] Timed out");
+                        }
+                        else {
+                            eprint!("Error: {:?}", e);
+                        }
+                    }
+                    index = 1;
+                }
+            }
+        }
+    }
     else {
-        eprintln!("Usage: {} [server|client] <port>", args[0]);
+        eprintln!("Usage: {} [server|client|proxy] <port>", args[0]);
         std::process::exit(1);
     }
 
@@ -78,5 +131,13 @@ fn init_client(port: u16) -> net::UdpSocket {
         .expect("Could not bind to address");
     socket.connect(net::SocketAddr::from(([127, 0, 0, 1], port)))
         .expect("Could not connect to server");
+    socket
+}
+
+fn init_proxy(port: u16) -> net::UdpSocket {
+    //initialize the proxy at localhost and the port number
+    let socket = net::UdpSocket::bind(net::SocketAddr::from(([127, 0, 0, 1], port)))
+        .expect("Could not bind to address");
+    println!("Proxy listening on port {}", port);
     socket
 }
